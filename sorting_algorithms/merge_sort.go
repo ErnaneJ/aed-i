adb8eea495be2d6bc0d07347620057e605902edd
Ernane Ferreira <ernane.junior25@gmail.com>
2023-04-21 13:11:06 -0300

    feat: implementing mergesort

---
+++ sorting_algorithms/merge_sort.go
@@ -0,0 +1,48 @@
+package main
+
+func MergeSort(list []int) {
+	if len(list) <= 1 {
+		return
+	}
+
+	mid := len(list) / 2
+
+	left := make([]int, mid)
+	right := make([]int, len(list[mid:]))
+
+	copy(left, list[:mid])
+	copy(right, list[mid:])
+
+	MergeSort(left)
+	MergeSort(right)
+
+	merge(list, left, right)
+}
+
+func merge(list []int, left []int, right []int) {
+	lenLeft, lenRight := len(left), len(right)
+	i, j, k := 0, 0, 0
+
+	for i < lenLeft && j < lenRight {
+		if left[i] < right[j] {
+			list[k] = left[i]
+			i++
+		} else {
+			list[k] = right[j]
+			j++
+		}
+		k++
+	}
+
+	for i < lenLeft {
+		list[k] = left[i]
+		k++
+		i++
+	}
+
+	for j < lenRight {
+		list[k] = right[j]
+		k++
+		j++
+	}
+}
--- sorting_algorithms/sorting_algorithms_test.go
+++ sorting_algorithms/sorting_algorithms_test.go
@@ -5,32 +5,41 @@ import (
 )
 
 func TestBubbleSort(t *testing.T) {
-	list := []int{9, 5, 1, 4, 7, 8, 6, 3, 2}
+	list := []int{9, 5, 1, 4, 0, 7, 8, 6, 3, 2}
 
 	BubbleSort(list)
 
-	if list[0] != 1 || list[len(list)-1] != 9 {
-		t.Errorf("BubbleSort: returned %d, but we expected it to be %d", list, []int{1, 2, 3, 4, 5, 6, 7, 8, 9})
+	if list[0] != 0 || list[len(list)-1] != 9 {
+		t.Errorf("BubbleSort: returned %d, but we expected it to be %d", list, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
 	}
 }
 
 func TestSelectionSort(t *testing.T) {
-	list := []int{9, 5, 1, 4, 7, 8, 6, 3, 2}
+	list := []int{9, 5, 1, 4, 0, 7, 8, 6, 3, 2}
 
 	SelectionSort(list)
 
-	if list[0] != 1 || list[len(list)-1] != 9 {
-		t.Errorf("SelectionSort: returned %d, but we expected it to be %d", list, []int{1, 2, 3, 4, 5, 6, 7, 8, 9})
+	if list[0] != 0 || list[len(list)-1] != 9 {
+		t.Errorf("SelectionSort: returned %d, but we expected it to be %d", list, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
 	}
 }
 
 func TestInsertionSort(t *testing.T) {
-	list := []int{9, 5, 1, 4, 7, 8, 6, 3, 2} // => 36
-	// list := []int{2, 1, 3, 4, 5, 6, 7, 8, 9} // => 15
+	list := []int{9, 5, 1, 4, 0, 7, 8, 6, 3, 2}
 
 	InsertionSort(list)
 
-	if list[0] != 1 || list[len(list)-1] != 9 {
-		t.Errorf("SelectionSort: returned %d, but we expected it to be %d", list, []int{1, 2, 3, 4, 5, 6, 7, 8, 9})
+	if list[0] != 0 || list[len(list)-1] != 9 {
+		t.Errorf("SelectionSort: returned %d, but we expected it to be %d", list, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+	}
+}
+
+func TestMergeSort(t *testing.T) {
+	list := []int{0, 2, 1, 3, 4, 5, 6, 7, 8, 9}
+
+	MergeSort(list)
+
+	if list[0] != 0 || list[len(list)-1] != 9 {
+		t.Errorf("MergeSort: returned %d, but we expected it to be %d", list, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
 	}
 }
